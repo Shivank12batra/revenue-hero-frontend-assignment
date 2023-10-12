b95f19652d3ab5316206e358bd5beec46889eedb
Shivank12batra <Shivank56batra@gmail.com>
2023-10-12 06:12:13 +0530

    refactor: calling the randomData generation function directly from serverProps

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import NavBar from "@/components/NavBar";
 import Dashboard from "@/components/Dashboard";
+import { generateRandomDataArray } from "@/randomData";
 
 export default function Home({data}) {
   console.log(data)
@@ -13,9 +14,9 @@ export default function Home({data}) {
 
 export async function getServerSideProps() {
   // Fetch data from the separate API route
-  const response = await fetch(process.env.NEXT_PUBLIC_API_KEY);
-  const data = await response.json();
-
+  // const response = await fetch(process.env.NEXT_PUBLIC_API_KEY);
+  // const data = await response.json();
+  const data = generateRandomDataArray(25);
   return {
     props: {
       data,
